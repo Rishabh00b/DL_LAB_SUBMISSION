1a49232efd0201f2bd6d9eb3512cbe1d97d9cc3e
RISHABH <Rishabh00b@gmail.com>
2026-02-13 09:48:01 +0530

    Updated DL_LAB1

---
+++ DL_LAB1/check_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+a=np.array([1,2])
+print(a)
+b=np.array([1,2])
+
+print(b)
+print("weighted sum:",np.dot(a, b))
+
+
+# import numpy as np
+
+w = np.array([0.2, 0.4])
+x = np.array([1.0, 2.0])
+
+print("Weighted sum:", np.dot(w, x))
